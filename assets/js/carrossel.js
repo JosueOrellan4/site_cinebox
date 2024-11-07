bd99ba8d6adb5cd0225aa313fa445d95bb92f0c8
JosueOrellan4 <tomiokaorellana@gmail.com>
2024-11-07 11:56:14 -0300

    atualização aula perdida

--- assets/js/carrossel.js
+++ assets/js/carrossel.js
@@ -1,33 +1,33 @@
-let indexSlide = 0;
+let indexSlide = 0; 
 function showSlide(index){
     const carrossel = document.querySelector('.carrossel')
-    const totalSlides = document.querySelectorAll('.carrossel img').length
+    // queryselector quero selecionar
+    const totalSlides= document.querySelectorAll('.carrossel img').length
+    // lenght conta quantos elementos tem 
 
-    if (index >= totalSlides) {
-        indexSlide = 0
-    } else if (index < 0){
+    if (index >= totalSlides){
+        indexSlide = 0 
+    }else if (index < 0 ){
         indexSlide = totalSlides - 1
-    } else {
+    }else{
         indexSlide = index
     }
-
-
     const offset = -indexSlide*100
-
     carrossel.style.transform = `translateX(${offset}%)`
-
 }
 
 function proximoSlide(){
-    indexSlide = indexSlide + 1
+    indexSlide = indexSlide + 1 
     showSlide(indexSlide)
 }
 
-function voltarSlide(){
-    indexSlide = indexSlide - 1
+function anteriorSlide(){
+    indexSlide = indexSlide - 1 
     showSlide(indexSlide)
 }
 
 setInterval(() => {
+// funçao anonima que so funciona para so aquilo e nada mais, pequena coisas
     proximoSlide()
-}, 5000)
+},2000)
+// setiternal -> ele executa a cada x tempo, pede para ser passado uma função para ele 
